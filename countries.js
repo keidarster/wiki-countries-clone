cb132212f11cba134c053a915abe66fd19bd80d0
Michael K <keidarster@gmail.com>
2018-02-20 15:47:58 +0100

    added a search bar for finding a specific country by name

--- countries.js
+++ countries.js
@@ -6,10 +6,15 @@ class countriesList {
       xhttp.send();
       var response = JSON.parse(xhttp.responseText);
       this.countries = response;
-      this.string = document.getElementById("tbody")
+      this.string = document.getElementById("tbody");
+      this.searchEventListener();
   }
   
   printCountries() {
+    // Search bar for specific countries
+    if(document.getElementById("searchcountry").value !== ""){
+      this.countries = this.countries.filter(i => i.name === document.getElementById("searchcountry").value)
+    }
     // Sorting the population ascending/descending respectively to the query string
     if (window.location.search.substr(1) === "pop=asc") {
       this.countries = this.countries.sort(function (a, b){return a.population - b.population})
@@ -17,8 +22,9 @@ class countriesList {
     else if (window.location.search.substr(1) === "pop=desc") {
       this.countries = this.countries.sort(function (a, b){return b.population - a.population})
     } 
+    var string = "";
     for (var i = 0; i < this.countries.length; i++) {
-      this.string.innerHTML += 
+      string += 
         `<tr>
           <td>${i+1}</td>
           <td> <img src="${this.countries[i].flag}" height="20" width="35">${this.countries[i].name}</td>
@@ -26,6 +32,12 @@ class countriesList {
           <td>${(this.countries[i].population / 7600000000 * 100).toFixed(4)}%</td>
         </tr>`;
     }
+    this.string.innerHTML = string;
+  }
+  searchEventListener () {
+    document.getElementById("searchbtn").addEventListener("click", e => {
+      this.printCountries();
+    })
   }
 }
 
