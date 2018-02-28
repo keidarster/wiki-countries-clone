846a62db9f1a30f57a6e0fbccd4efb98548783ac
Michael K <keidarster@gmail.com>
2018-02-28 13:52:15 +0100

    added a capital city column

--- src/countries.js
+++ src/countries.js
@@ -6,7 +6,6 @@ class countriesList {
       xhttp.send();
       var response = JSON.parse(xhttp.responseText);
       this.countries = response;
-      console.log(this.countries)
       this.string = document.getElementById("tbody");
       this.searchEventListener();
   }
@@ -29,6 +28,7 @@ class countriesList {
         `<tr>
           <td>${i+1}</td>
           <td> <img src="${this.countries[i].flag}" height="20" width="35">${this.countries[i].name}</td>
+          <td>${this.countries[i].capital}</td>
           <td>${this.countries[i].population}</td>
           <td>${(this.countries[i].population / 7600000000 * 100).toFixed(4)}%</td>
         </tr>`;
